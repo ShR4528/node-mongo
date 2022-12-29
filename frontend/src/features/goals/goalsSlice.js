016fd5b8a7458ab32b8ad6fba1eeee1839cfbef9
Shr4528 <Shr4528@gmail.com>
2022-12-29 18:58:52 +0200

    commi

--- frontend/src/features/goals/goalsSlice.js
+++ frontend/src/features/goals/goalsSlice.js
@@ -1,5 +1,5 @@
-import React from 'react'
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import goalService from './goalService'
 
 const initialState = {
     goals: [],
@@ -9,12 +9,44 @@ const initialState = {
     message: '',
 }
 
+export const createGoal = createAsyncThunk('goals/create',
+    async (goalData, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await goalService.createGoal(goalData, token)
+        } catch (error) {
+            const message = (error.response &&
+                error.response.data &&
+                error.response.message) ||
+                error.message ||
+                error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    })
+
 
 export const goalsSlice = createSlice({
     name: 'goal',
     initialState,
     reducers: {
         reset: (state) => initialState
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createGoal.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(createGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals.push(action.payload)
+            })
+            .addCase(createGoal.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+
+            })
     }
 })
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import GoalForm from "../components/GoalForm"
 
 function Dashboard() {
   const navigate = useNavigate()
@@ -10,7 +10,7 @@ function Dashboard() {
 
   useEffect(() => {
     if (!user) {
-      navigate('/login')
+      navigate("/login")
     }
   }, [user, navigate])
 
@@ -18,7 +18,9 @@ function Dashboard() {
     <>
       <section className="heading">
         <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
       </section>
+      <GoalForm />
     </>
   )
 }
